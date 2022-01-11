496b366835c940b782748504fbaead8d21b44679
201851019-iiitv <201851019@iiitvadodara.ac.in>
2022-01-11 21:44:11 +0530

    update some files

--- UserManagement/src/main/java/ambesh/UserManagement/controller/UserController.java
+++ UserManagement/src/main/java/ambesh/UserManagement/controller/UserController.java
@@ -4,6 +4,7 @@ import ambesh.UserManagement.exception.ResourceNotFoundException;
 import ambesh.UserManagement.model.User;
 import ambesh.UserManagement.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,29 +18,51 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    // ToDo: handle validation(unique id,username,mobileno,email)
+
     @PostMapping
     public User createUser(@RequestBody User user) {
-        return (User) userRepository.save(user);
+        return userRepository.save(user);
     }
 
 
-    @GetMapping("{userId}")
+    @GetMapping
     public  ResponseEntity<User> getUserById(@RequestParam long userId){
+        System.out.println(userId);
         User user = userRepository.findById(userId)
-                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id:" + userId));
+                .orElseThrow(() -> new ResourceNotFoundException("User not exist with id:" + userId));
         return ResponseEntity.ok(user);
     }
 
-//
-//    @GetMapping
-//    public List<User> getAllUser(){
-//        return userRepository.findAll();
-//    }
+ @PutMapping
+ public ResponseEntity<User> updateEmployee(@RequestParam long userId,@RequestBody User userDetails) {
+     User updateUser = userRepository.findById(userId)
+             .orElseThrow(() -> new ResourceNotFoundException("User not exist with id: " + userId));
+
+     updateUser.setUserName(userDetails.getUserName());
+     updateUser.setFirstName(userDetails.getFirstName());
+     updateUser.setLastName(userDetails.getLastName());
+     updateUser.setMobileNumber(userDetails.getMobileNumber());
+     updateUser.setEmailID(userDetails.getEmailID());
+     updateUser.setAddress1(userDetails.getAddress1());
+     updateUser.setAddress2(userDetails.getAddress2());
 
 
+     userRepository.save(updateUser);
 
+     return ResponseEntity.ok(updateUser);
+ }
 
+    @DeleteMapping
+    public ResponseEntity<HttpStatus> deleteEmployee(@RequestParam long userId){
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id: " + userId));
+
+        userRepository.delete(user);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+    }
 
 
 
--- UserManagement/src/main/java/ambesh/UserManagement/repository/UserRepository.java
+++ UserManagement/src/main/java/ambesh/UserManagement/repository/UserRepository.java
@@ -4,12 +4,5 @@ import ambesh.UserManagement.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Long> {
-    //List<User> findByFirstName(String firstName);
-    //    save
-    //    findOne
-   //    exists
-   //    findAll
-  //    count
-  //    delete
-  //    deleteAll
+    // all crud database methods
 }
