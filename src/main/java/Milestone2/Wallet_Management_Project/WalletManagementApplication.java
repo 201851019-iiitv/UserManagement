7266496112e853fd5730e06e0513f17eb9524aae
201851019-iiitv <201851019@iiitvadodara.ac.in>
2022-01-27 21:18:14 +0530

    testing github work on mac

--- src/main/java/Milestone2/Wallet_Management_Project/WalletManagementApplication.java
+++ src/main/java/Milestone2/Wallet_Management_Project/WalletManagementApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WalletManagementApplication {
 
+	//spring Runner class
 	public static void main(String[] args) {
 		SpringApplication.run(WalletManagementApplication.class, args);
 	}
